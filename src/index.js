f61bdabe8d19465897d41ac41a9af4d1507b5ea3
guilhermebehs <guilhermebehs2013@hotmail.com>
2021-03-24 14:19:01 -0300

    Manipulados argumentos de linha de comando e criado socket client

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import Events from 'events';
+import CliConfig from './cliConfig.js';
+import SocketClient from './socket.js';
 import TerminalController from './terminalController.js';
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+//const controller = new TerminalController();
+//await controller.initializeTable(componentEmitter);
