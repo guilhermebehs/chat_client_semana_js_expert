a53254a7dcc024000129cea8eac7b6a75fce288e
Guilherme <guilhermebehs2013@hotmail.com>
2021-03-26 08:48:47 -0300

    Liga os eventos ao socket

--- src/socket.js
+++ src/socket.js
@@ -1,9 +1,43 @@
+import Event from 'events';
 export default class SocketClient {
-    #serverConnection
+    #serverConnection = {}
+    #serverListener = new Event();
     constructor({host, port, protocol}){
          this.host = host;
          this.port = port;
          this.protocol = protocol;
+    }
+
+    sendMessage(event, message){
+        this.#serverConnection.write(JSON.stringify({event, message}))
+    }
+
+    attachEvents(events){
+        this.#serverConnection.on('data', data => {
+              try{
+                data.toString()
+                    .split('\n')
+                    .filter(line => !!line)
+                    .map(JSON.parse)
+                    .map(({event, message})=>{
+                       this.#serverListener.emit(event, message)
+                    })
+                }
+                catch(e){
+                    console.log('Invalid!', data.toString(), error)
+                }
+        })
+
+        this.#serverConnection.on('end', ()=>{
+            console.log('I disconnected!!')
+        })
+        this.#serverConnection.on('error', ()=>{
+            console.log('Deu ruim', error)
+        })
+
+        for(const [key, value] of events){
+            this.#serverListener.on(key, value)
+        }
     }
 
     async createConnection (){
@@ -22,8 +56,11 @@ export default class SocketClient {
             req.once('upgrade', (res, socket)=> resolve(socket))
         })
         }
+
     async initialize(){
         this.#serverConnection = await this.createConnection();
         console.log('I connected to the server')
-    }
+       }
+
+
     }
