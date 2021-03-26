ed96b6ffc3671ec55f7a2738da9610b3a6af00c2
Guilherme <guilhermebehs2013@hotmail.com>
2021-03-26 08:47:22 -0300

    Incluídas constantes de socket

--- src/constants.js
+++ src/constants.js
@@ -6,5 +6,9 @@ export const constants = {
           ACTIVITYLOG_UPDATED: 'activityLog:updated',
           STATUS_UPDATED: 'status:updated',
         },
+        socket:{
+          JOIN_ROOM: 'joinRoom',
+          MESSAGE: 'message'
+        }
     }
 }
